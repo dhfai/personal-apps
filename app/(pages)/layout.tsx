59918edbdad7cd3b457e0a749a6702a6cdbf89fb
dhfai <105841108621@student.unismuh.ac.id>
2024-11-23 20:47:15 +0800

    repair setup

---
+++ app/(pages)/layout.tsx
@@ -0,0 +1,18 @@
+import styles from '@/styles/module/layout.module.scss';
+
+export default function layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className={styles.container}>
+      {/* Navigation Bar */}
+      <header className={styles.navbar}>
+        <div className={styles.logo}>LOGO</div>
+        <div className={styles.hamburger}>
+          <div />
+          <div />
+          <div />
+        </div>
+      </header>
+      {children}
+    </div>
+  );
+}
